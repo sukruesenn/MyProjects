241c75358a53933d43185971f5b410fe12a4835b
sukruesenn <93861570+sukruesenn@users.noreply.github.com>
2021-12-29 23:12:15 +0300

    Add files via upload

--- sukruesen/js/script.js
+++ sukruesen/js/script.js
@@ -1,8 +1,39 @@
+var x = 0;
 //Navbar
 let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar .links');
+let navbarLinks = document.querySelector('.header .navbar .links');
+let navbar = document.querySelector('.navbar');
+let fatimes = document.querySelector('.fa-times');
 
 menu.onclick = () =>{
 	menu.classList.toggle('fa-times');
-	navbar.classList.toggle('active');
+	navbarLinks.classList.toggle('active');
+	x += 1;
+	if (window.scrollY < 1 && x == 1){
+		navbar.style.background = "rgba(0,0,0,.95)";
+	}else if (window.scrollY < 1 && x == 2){
+		navbar.style.background = "transparent";
+		x = 0;
+	}
 }
+
+navbarLinks.onclick = () =>{
+	menu.classList.remove('fa-times');
+	navbarLinks.classList.remove('active');
+	if (window.scrollY < 1){
+		navbar.style.background = "transparent";
+		x = 0;
+	}
+}
+
+window.onscroll = () =>{
+	if (window.scrollY > 0){
+		navbar.classList.add('active');
+		navbar.style.background = "#fff";
+	}else{
+		navbar.classList.remove('active');
+		navbar.style.background = "transparent";
+		x = 0;
+	}
+}
+
